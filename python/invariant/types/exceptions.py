0307c4ed236b8016c8e8a01e8a9da917cfbb688d
Hemang <hemang@invariantlabs.ai>
2024-10-10 11:36:24 +0200

    Add exceptions.

--- python/invariant/types/exceptions.py
+++ python/invariant/types/exceptions.py
@@ -1,1 +1,21 @@
 """Describes the exceptions that can be raised by the Invariant SDK."""
+
+
+class InvariantError(Exception):
+    """An error occurred while communicating with the Invariant API."""
+
+
+class InvariantAPIError(InvariantError):
+    """Internal server error while communicating with Invariant."""
+
+
+class InvariantUserError(InvariantError):
+    """User error caused an exception when communicating with Invariant."""
+
+
+class InvariantAuthError(InvariantError):
+    """Couldn't authenticate with the Invariant API."""
+
+
+class InvariantNotFoundError(InvariantError):
+    """Couldn't find the requested resource."""
