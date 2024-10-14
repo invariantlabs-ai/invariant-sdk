ffcd03ec0abbb8a206cf9cb8925a502479d71087
Hemang <hemang@invariantlabs.ai>
2024-10-14 16:43:03 +0200

    Add some more tests.

---
+++ python/tests/unit-tests/test_client.py
@@ -0,0 +1,291 @@
+"""Unit tests for the Client class."""
+
+from unittest import mock
+
+import pytest
+import requests
+from invariant.client import Client
+from invariant.types.annotations import AnnotationCreate
+from invariant.types.push_traces import PushTracesRequest
+from invariant.types.exceptions import (
+    InvariantAPITimeoutError,
+    InvariantNotFoundError,
+    InvariantAuthError,
+    InvariantAPIError,
+    InvariantError,
+)
+
+
+@pytest.fixture(name="set_env_vars")
+def fixture_set_env_vars(monkeypatch):
+    """Fixture to set environment variables for tests."""
+    monkeypatch.setenv("INVARIANT_API_ENDPOINT", "https://default.api.url")
+    monkeypatch.setenv("INVARIANT_API_KEY", "test-key")
+
+
+@pytest.fixture(name="push_traces_request")
+def fixture_push_traces_request():
+    """Fixture for a PushTracesRequest instance."""
+    return PushTracesRequest(
+        messages=[
+            [
+                {"role": "user", "content": "one"},
+                {"role": "assistant", "content": "two \n three"},
+            ]
+        ],
+        annotations=[
+            [
+                AnnotationCreate(
+                    content="annotating one",
+                    address="messages[0].content:L0",
+                    extra_metadata={"key1": "value1"},
+                )
+            ]
+        ],
+        dataset="example_dataset",
+    )
+
+
+def test_client_init_defaults(set_env_vars):  # pylint: disable=unused-argument
+    """Test Client initialization with default values."""
+    client = Client()
+
+    assert client.api_url == "https://default.api.url"
+    assert client.api_key == "test-key"
+    assert client.timeout_ms == (5000, 20000)
+    assert client._headers == {  # pylint: disable=protected-access
+        "Authorization": "Bearer test-key",
+        "Accept": "application/json",
+    }
+
+
+def test_client_init_custom_values():
+    """Test Client initialization with custom values."""
+    custom_timeout = (10000, 30000)
+    custom_session = requests.Session()
+
+    client = Client(
+        api_url="https://default.api.url",
+        api_key="test-key",
+        timeout_ms=custom_timeout,
+        session=custom_session,
+    )
+
+    assert client.api_url == "https://default.api.url"
+    assert client.api_key == "test-key"
+    assert client.timeout_ms == custom_timeout
+    assert client.session == custom_session
+
+
+def test_client_init_single_timeout_value(set_env_vars):  # pylint: disable=unused-argument
+    """Test Client initialization with a single timeout value."""
+    custom_timeout = 15000
+
+    client = Client(
+        api_url="https://default.api.url", api_key="test-key", timeout_ms=custom_timeout
+    )
+
+    assert client.timeout_ms == (custom_timeout, custom_timeout)
+
+
+def test_client_repr():
+    """Test the __repr__ method of the Client class."""
+    client = Client(api_url="https://custom.api.url", api_key="test")
+    expected_repr = "Invariant Client API URL: https://custom.api.url"
+    assert repr(client) == expected_repr
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_traces(mock_session_cls: mock.Mock, set_env_vars, push_traces_request):  # pylint: disable=unused-argument
+    """Test the push_traces method."""
+    mock_response = mock.Mock()
+    mock_response.json.return_value = {
+        "id": ["123"],
+        "dataset": "example_dataset",
+    }
+    mock_session = mock.Mock()
+    mock_session.request.return_value = mock_response
+    mock_session_cls.return_value = mock_session
+
+    client = Client(timeout_ms=(3000, 7000))
+    push_traces_response = client.push_trace(push_traces_request)
+    assert push_traces_response.id == ["123"]
+    assert push_traces_response.dataset == "example_dataset"
+
+    # Assert that the request method was called once with the expected arguments.
+    mock_session.request.assert_called_once_with(
+        method="POST",
+        url="https://default.api.url/api/v1/push/trace",
+        json=push_traces_request.to_json(),
+        timeout=(3.0, 7.0),
+        headers={
+            "Authorization": "Bearer test-key",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        stream=False,
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_timeout(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a timeout raises InvariantAPITimeoutError."""
+    mock_session = mock.Mock()
+    mock_session.request.side_effect = requests.ReadTimeout
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantAPITimeoutError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Timeout when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_connection_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a ConnectionError raises InvariantError."""
+    mock_session = mock.Mock()
+    mock_session.request.side_effect = requests.ConnectionError
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Connection error when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_connect_timeout_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a ConnectTimeout raises InvariantError."""
+    mock_session = mock.Mock()
+    mock_session.request.side_effect = requests.ConnectTimeout
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Connection error when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_auth_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that an authentication error raises InvariantAuthError."""
+    mock_response = mock.Mock()
+    mock_response.raise_for_status.side_effect = requests.HTTPError
+    mock_response.status_code = 401
+    mock_session = mock.Mock()
+    mock_session.request.return_value = mock_response
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantAuthError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Authentication failed when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_not_found_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a not found error raises InvariantNotFoundError."""
+    mock_response = mock.Mock()
+    mock_response.raise_for_status.side_effect = requests.HTTPError
+    mock_response.status_code = 404
+    mock_session = mock.Mock()
+    mock_session.request.return_value = mock_response
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantNotFoundError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Resource not found when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_too_many_requests_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a too many requests error raises InvariantError."""
+    mock_response = mock.Mock()
+    mock_response.raise_for_status.side_effect = requests.HTTPError
+    mock_response.status_code = 429
+    mock_session = mock.Mock()
+    mock_session.request.return_value = mock_response
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Error calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_server_side_error(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a server side error raises InvariantAPIError."""
+    mock_response = mock.Mock()
+    mock_response.raise_for_status.side_effect = requests.HTTPError
+    mock_response.status_code = 500
+    mock_session = mock.Mock()
+    mock_session.request.return_value = mock_response
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantAPIError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Server error caused failure when calling method: POST for path: /api/v1/push/trace."
+    )
+
+
+@mock.patch("invariant.client.requests.Session")
+def test_push_trace_generic_exception(
+    mock_session_cls: mock.Mock, set_env_vars, push_traces_request
+):  # pylint: disable=unused-argument
+    """Test that a generic exception raises InvariantError."""
+    mock_session = mock.Mock()
+    mock_session.request.side_effect = Exception("Generic error")
+    mock_session_cls.return_value = mock_session
+
+    client = Client()
+
+    with pytest.raises(InvariantError) as exc_info:
+        client.push_trace(push_traces_request)
+    assert (
+        str(exc_info.value)
+        == "Error calling method: POST for path: /api/v1/push/trace."
+    )
---
+++ python/tests/unit-tests/types/test_push_traces.py
@@ -0,0 +1,193 @@
+"""Tests for push_traces.py"""
+
+from typing import Dict, List
+import pytest
+from invariant.types.annotations import AnnotationCreate
+from invariant.types.push_traces import PushTracesRequest, PushTracesResponse
+
+
+@pytest.fixture(name="valid_messages")
+def fixture_valid_messages() -> List[List[Dict[str, str]]]:
+    """Fixture for valid messages."""
+    return [[{"key1": "value1"}, {"key2": "value2"}]]
+
+
+@pytest.fixture(name="valid_annotations")
+def fixture_valid_annotations() -> List[List[AnnotationCreate]]:
+    """Fixture for valid annotations."""
+    return [
+        [AnnotationCreate(content="example content", address="messages[0].content:L0")]
+    ]
+
+
+@pytest.fixture(name="valid_metadata")
+def fixture_valid_metadata() -> List[Dict[str, str]]:
+    """Fixture for valid metadata."""
+    return [{"meta_key": "meta_value"}]
+
+
+def test_validate_fields_request_valid_data(
+    valid_messages, valid_annotations, valid_metadata
+):
+    """Test PushTracesRequest.validate_fields with valid data."""
+    # Should not raise any exception
+    request = PushTracesRequest(
+        messages=valid_messages,
+        annotations=valid_annotations,
+        metadata=valid_metadata,
+        dataset="example dataset",
+    )
+
+    assert (
+        request.messages == valid_messages
+        and request.annotations == valid_annotations
+        and request.metadata == valid_metadata
+        and request.dataset == "example dataset"
+    )
+
+
+def test_validate_fields_request_invalid_messages(valid_annotations, valid_metadata):
+    """Test PushTracesRequest.validate_fields with invalid messages."""
+    invalid_messages = [{"key1": "value1"}, {"key2": "value2"}]
+
+    with pytest.raises(
+        ValueError, match="messages must be a list of lists of dictionaries"
+    ):
+        _ = PushTracesRequest(
+            messages=invalid_messages,
+            annotations=valid_annotations,
+            metadata=valid_metadata,
+        )
+
+    with pytest.raises(ValueError, match="messages cannot be empty"):
+        _ = PushTracesRequest(
+            messages=None, annotations=valid_annotations, metadata=valid_metadata
+        )
+
+    with pytest.raises(ValueError, match="messages cannot be empty"):
+        _ = PushTracesRequest(
+            messages=[], annotations=valid_annotations, metadata=valid_metadata
+        )
+
+
+def test_validate_fields_request_invalid_annotations(valid_messages, valid_metadata):
+    """Test PushTracesRequest.validate_fields with invalid annotations."""
+    invalid_annotations = []
+
+    with pytest.raises(
+        ValueError, match="annotations must be the same length as messages"
+    ):
+        _ = PushTracesRequest(
+            messages=valid_messages,
+            annotations=invalid_annotations,
+            metadata=valid_metadata,
+        )
+    with pytest.raises(
+        ValueError, match="annotations must be a list of lists of AnnotationCreate type"
+    ):
+        _ = PushTracesRequest(
+            messages=valid_messages,
+            annotations=[
+                {"content": "example content", "addres": "messages[0].content:L0"}
+            ],
+            metadata=valid_metadata,
+        )
+
+
+def test_validate_fields_request_invalid_metadata(valid_messages, valid_annotations):
+    """Test PushTracesRequest.validate_fields with invalid metadata."""
+    invalid_metadata = [{"meta_key": "meta_value"}, {"meta_key": "meta_value"}]
+
+    with pytest.raises(
+        ValueError, match="metadata must be the same length as messages"
+    ):
+        _ = PushTracesRequest(
+            messages=valid_messages,
+            annotations=valid_annotations,
+            metadata=invalid_metadata,
+        )
+
+
+def test_validate_fields_request_none_values(valid_messages):
+    """Test PushTracesRequest.validate_fields with None values."""
+    # Should not raise any exception
+    request = PushTracesRequest(
+        messages=valid_messages, annotations=None, metadata=None
+    )
+
+    assert (
+        request.messages == valid_messages
+        and request.annotations is None
+        and request.metadata is None
+    )
+
+
+def test_request_to_json(valid_messages, valid_annotations, valid_metadata):
+    """Test converting PushTracesRequest to a JSON object."""
+    request = PushTracesRequest(
+        messages=valid_messages,
+        annotations=valid_annotations,
+        metadata=valid_metadata,
+        dataset="example_dataset",
+    )
+    json_obj = request.to_json()
+
+    assert json_obj == {
+        "messages": [
+            [
+                {
+                    "key1": "value1",
+                },
+                {
+                    "key2": "value2",
+                },
+            ],
+        ],
+        "annotations": [
+            [
+                {
+                    "address": "messages[0].content:L0",
+                    "extra_metadata": None,
+                    "content": "example content",
+                },
+            ],
+        ],
+        "dataset": "example_dataset",
+        "metadata": [
+            {
+                "meta_key": "meta_value",
+            },
+        ],
+    }
+
+
+def test_response_from_json():
+    """Test creating PushTracesResponse from a JSON object."""
+    json_obj = {"id": ["123", "456"], "dataset": "example_dataset"}
+    response = PushTracesResponse.from_json(json_obj)
+
+    assert response.id == ["123", "456"]
+    assert response.dataset == "example_dataset"
+
+
+def test_response_from_json_missing_dataset():
+    """Test creating PushTracesResponse from a JSON object without dataset."""
+    json_obj = {"id": ["123", "456"]}
+    response = PushTracesResponse.from_json(json_obj)
+
+    assert response.id == ["123", "456"] and response.dataset is None
+
+
+def test_response_from_json_empty_id():
+    """Test creating PushTracesResponse from a JSON object with empty id list."""
+    json_obj = {"id": [], "dataset": "example_dataset"}
+    response = PushTracesResponse.from_json(json_obj)
+
+    assert response.id == [] and response.dataset == "example_dataset"
+
+
+def test_response_from_invalid_json():
+    """Test creating PushTracesResponse from an invalid JSON object."""
+    json_obj = {"invalid_field": "invalid_value"}
+    with pytest.raises(ValueError):
+        PushTracesResponse.from_json(json_obj)
